54a806d871a70de95a7f9c17422372a45cfa6de0
Christopher <4308353+turleynerd@users.noreply.github.com>
2020-04-24 13:54:00 -0400

    Updated composer and certs gen

--- generate-certs.sh
+++ generate-certs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # create the root key
-openssl genrsa -aes256 -out certs/root.key 4096
+openssl genrsa -out certs/root.key 4096
 
 # create and self sign the root certificate
 openssl req -x509 -new -nodes -key certs/root.key -sha256 -days 1024 -out certs/root.crt
 
 # create the certiicate key
-openssl genrsa -out certs/make-production.internal.key 2048
+openssl genrsa -out certs/dev.internal.key 2048
 
 # create the signing request
-openssl req -new -sha256 -key certs/make-production.internal.key -subj "/C=US/ST=NC/O=Red Ventures/CN=nsqd" -out certs/make-production.internal.csr
+openssl req -new -sha256 -key certs/dev.internal.key -subj "/C=US/ST=NC/O=Red Ventures/CN=nsqd" -out certs/dev.internal.csr
 
 # create the certificate using the csr and certificate key along with the root key
-openssl x509 -req -in certs/make-production.internal.csr -CA certs/root.crt -CAkey certs/root.key -CAcreateserial -out certs/make-production.internal.crt -days 500 -sha256
+openssl x509 -req -in certs/dev.internal.csr -CA certs/root.crt -CAkey certs/root.key -CAcreateserial -out certs/dev.internal.crt -days 500 -sha256
